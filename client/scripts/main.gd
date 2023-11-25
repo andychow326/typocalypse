a4e093701191123a94bda49579644b7995b66654
Andy Chow <andychchow326@gmail.com>
2023-11-25 18:54:16 +0800

    client: Add exponential backoff reconnect approach

--- client/scripts/main.gd
+++ client/scripts/main.gd
@@ -1,6 +1,9 @@
 extends Node
 
 
+var web_socket_connection_closed_count := 0
+
+
 func _ready():
 	DataStore.web_socket_client.connected_to_server.connect(_on_web_socket_client_connected_to_server)
 	DataStore.web_socket_client.connection_closed.connect(_on_web_socket_client_connection_closed)
@@ -20,11 +23,18 @@ func restart_client():
 
 
 func _on_web_socket_client_connected_to_server():
+	web_socket_connection_closed_count = 0
 	print("[Client] Connected to server!")
 
 
 func _on_web_socket_client_connection_closed():
+	web_socket_connection_closed_count += 1
 	print("[Client] Connection closed.")
+	# Reconnect with exponential backoff
+	var timeout = int(pow(2, web_socket_connection_closed_count))
+	print("[Client] Reconnecting to server in %d seconds..." % timeout)
+	await get_tree().create_timer(timeout).timeout
+	start_client()
 
 
 func _on_web_socket_client_message_received(message):
