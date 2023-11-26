66e3741e1e08816fc5f7d8903a7667deaf2b6356
Andy Chow <andychchow326@gmail.com>
2023-11-26 21:01:58 +0800

    client: Implement game world player input event handler

--- client/scripts/world/player.gd
+++ client/scripts/world/player.gd
@@ -29,3 +29,21 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func _input(event):
+	if not get_parent().visible or not event is InputEventKey or not event.is_pressed():
+		return
+
+	if (event.keycode >= 65 and event.keycode <= 90) or event.keycode == 32:
+		var key_label
+		if event.keycode == KEY_SPACE:
+			key_label = " "
+		else:
+			key_label = OS.get_keycode_string(event.keycode).to_lower()
+		DataStore.web_socket_client.send({
+			"event": "input",
+			"data": {
+				"key": key_label
+			}
+		})
