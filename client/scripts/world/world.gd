f04c0a91186bf638fc11ea668bc3898b6829c9ff
Andy Chow <andychchow326@gmail.com>
2023-11-26 23:52:14 +0800

    client: Add game start countdown timer

--- client/scripts/world/world.gd
+++ client/scripts/world/world.gd
@@ -12,7 +12,15 @@ var player_positions = [
 
 
 func _ready():
+	$RoundStartLabel.visible = false
+	$RemainingTimeLabel.visible = false
 	DataStore.web_socket_client.message_received.connect(_on_web_socket_client_message_received)
+
+
+func _process(_delta):
+	$RemainingTimeLabel.text = "%.1f" % $RemainingTimeTimer.time_left
+	if not int($RoundStartTimer.time_left) == 0:
+		$RoundStartLabel.text = "%.f" % $RoundStartTimer.time_left
 
 
 func _on_web_socket_client_message_received(message):
@@ -20,8 +28,12 @@ func _on_web_socket_client_message_received(message):
 		return
 	match message.event:
 		"getRoomStatus":
+			if not visible:
+				return
 			reset_player_container()
 			var position_index = 0
+			$RoundStartTimer.wait_time = message.data.room.roundWaitDurationSeconds + 0.5
+			$RemainingTimeTimer.wait_time = message.data.room.roundDurationSeconds
 			for user in message.data.room.users.values():
 				var player_node = player_scene.instantiate()
 				player_node.position = player_positions[position_index]
@@ -29,6 +41,9 @@ func _on_web_socket_client_message_received(message):
 				player_node.from_dict(user)
 				$PlayerContainer.add_child(player_node)
 				position_index += 1
+			$RoundStartLabel.text = "READY"
+			await get_tree().create_timer(1).timeout
+			$RoundStartTimer.start()
 
 
 func reset_player_container():
@@ -47,3 +62,18 @@ func fetch_room_status():
 			"roomId": DataStore.room_id
 		}
 	})
+
+
+func _on_visibility_changed():
+	if visible:
+		$RoundStartLabel.visible = true
+		$RemainingTimeLabel.visible = false
+	if not visible:
+		$RoundStartTimer.stop()
+		$RemainingTimeTimer.stop()
+
+
+func _on_round_start_timer_timeout():
+	$RoundStartLabel.visible = false
+	$RemainingTimeLabel.visible = true
+	$RemainingTimeTimer.start()
