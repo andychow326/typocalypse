6f3b50f502712d6615c0b62f5e7ba61b97488ebe
Andy Chow <andychchow326@gmail.com>
2024-04-13 23:02:32 +0800

    Optimize world scene

--- client/scripts/world/world.gd
+++ client/scripts/world/world.gd
@@ -5,23 +5,19 @@ const Trie = preload("res://scripts/trie.gd")
 @export var player_scene: PackedScene
 @export var zombie_scene: PackedScene
 
-var gun_animation
-var gun_barrel
-
 var player_positions = [
 	Vector3(-1.5, 0, 16),
 	Vector3(1.5, 0, 16),
 	Vector3(-3, 0, 14),
 	Vector3(3, 0, 14),
 ]
+var player_id_to_player_node_id_map: Dictionary
 var player_tries: Dictionary
 var player_active_inputs: Dictionary
 var dead_zombies: Dictionary
-var last_potential_target_zombies: Array
+var last_potential_target_zombies: Dictionary
 var game_stated: bool
 var bullet = load("res://scenes/world/bullet.tscn")
-var bullet_instance
-var gun_instance
 
 
 func _ready():
@@ -67,7 +63,7 @@ func _on_web_socket_client_message_received(message):
 				words.append(word_item)
 				$ZombieContainer.add_child(zombie_node)
 
-			var index = 0
+			var position_index = 0
 			for user in message.data.room.users.values():
 				var user_words = words.filter(
 					func (word: Dictionary): return word.userId == user.id,
@@ -80,14 +76,15 @@ func _on_web_socket_client_message_received(message):
 				)
 				player_tries[user.id] = trie
 				player_active_inputs[user.id] = ""
+				last_potential_target_zombies[user.id] = []
+				player_id_to_player_node_id_map[user.id] = position_index
 
 				var player_node = player_scene.instantiate()
-				gun_animation = player_node.get_node("Head/Camera3D/Rifle/AnimationPlayer")
-				player_node.position = player_positions[index]
+				player_node.position = player_positions[position_index]
 				player_node.from_dict(user)
 				player_node.player_hit.connect(_on_player_hit)
 				$PlayerContainer.add_child(player_node)
-				index += 1
+				position_index += 1
 
 			$RoundStartLabel.text = "READY"
 			await get_tree().create_timer(1).timeout
@@ -120,9 +117,13 @@ func reset_active_zombies(indexs: Array):
 
 
 func on_player_inputted_key(player_id: String, key: String):
-	if !gun_animation.is_playing():
-		gun_animation.play("Shoot")
-		
+	if DataStore.player_id == player_id:
+		var player_node_id = player_id_to_player_node_id_map[player_id]
+		var player_node = $PlayerContainer.get_child(player_node_id)
+		var gun_animation = player_node.get_node("Head/Rifle/AnimationPlayer")
+		if !gun_animation.is_playing():
+			gun_animation.play("Shoot")
+
 	player_active_inputs[player_id] += key
 	var potential_target_zombies: Array = player_tries[player_id].get_potential_candidates(player_active_inputs[player_id])
 	potential_target_zombies = potential_target_zombies.filter(
@@ -132,35 +133,30 @@ func on_player_inputted_key(player_id: String, key: String):
 	if potential_target_zombies.is_empty():
 		player_active_inputs[player_id] = ""
 		reset_active_zombies(
-			last_potential_target_zombies.map(
+			last_potential_target_zombies[player_id].map(
 				func (item: Dictionary): return item.zombie_id
 			)
 		)
 		return
 
 	for zombie in potential_target_zombies:
-		var node = $ZombieContainer.get_child(zombie.zombie_id)
-		var player_node
-		var children = $PlayerContainer.get_children()
-		var index = 0
-		for child in children:
-			if child.player_id == zombie.userId:
-				player_node = $PlayerContainer.get_child(index)
-				break
-			index += 1
-		node.set_active(player_active_inputs[player_id])
+		var zombie_node = $ZombieContainer.get_child(zombie.zombie_id)
+		zombie_node.set_active(player_active_inputs[player_id])
 
-		gun_instance = player_node.get_node("Head/Camera3D/Rifle")
-		gun_barrel = player_node.get_node("Head/Camera3D/Rifle/RayCast3D")
-		bullet_instance = bullet.instantiate()
+		var player_node_id = player_id_to_player_node_id_map[zombie.userId]
+		var player_node = $PlayerContainer.get_child(player_node_id)
+
+		var gun_instance = player_node.get_node("Head/Rifle")
+		var gun_barrel = player_node.get_node("Head/Rifle/RayCast3D")
+		var bullet_instance = bullet.instantiate()
 		bullet_instance.position = gun_barrel.global_position
-		
-		var zombie_vector = player_node.position.direction_to(node.position)
+
+		var zombie_vector = player_node.position.direction_to(zombie_node.position)
 		var zombie_basis = player_node.basis.looking_at(zombie_vector)
 		bullet_instance.transform.basis = basis.slerp(zombie_basis, 1)
 		gun_instance.transform.basis = basis.slerp(zombie_basis, 1)
-		
-		var zombie_angle = atan2((node.position.x - player_node.position.x), (node.position.z - player_node.position.z))
+
+		var zombie_angle = atan2((zombie_node.position.x - player_node.position.x), (zombie_node.position.z - player_node.position.z))
 		var face_angle = atan2(player_node.position.x, player_node.position.z)
 		if (zombie_angle < 0):
 			if (zombie_angle + PI) >= 0.35:
@@ -176,38 +172,35 @@ func on_player_inputted_key(player_id: String, key: String):
 		# elif zombie_angle > 0:
 		# 	bullet_instance.rotate_y(zombie_angle + PI - face_angle)
 		get_parent().add_child(bullet_instance)
-	
+
 		if zombie.word == player_active_inputs[player_id]:
 			dead_zombies[zombie.zombie_id] = zombie
-			node.killed()
+			zombie_node.killed()
 			player_active_inputs[player_id] = ""
 
-	var inactive_zombie_ids = last_potential_target_zombies.filter(
+	var inactive_zombie_ids = last_potential_target_zombies[player_id].filter(
 		func (item1: Dictionary): return not potential_target_zombies.any(
 			func (item2: Dictionary): return item1.zombie_id == item2.zombie_id
 		)
 	).map(func (item: Dictionary): return item.zombie_id)
 	reset_active_zombies(inactive_zombie_ids)
-	last_potential_target_zombies = potential_target_zombies.filter(
+	last_potential_target_zombies[player_id] = potential_target_zombies.filter(
 		func (zombie: Dictionary): return not dead_zombies.has(zombie.zombie_id)
 	)
 
 func _physics_process(delta):
 	if not game_stated:
 		return
-		
+
 	var zombie_child = $ZombieContainer.get_children()
 	var player_child = $PlayerContainer.get_children()
-	var index = 0
 	for zombie in zombie_child:
 		for player in player_child:
 			if player.player_id == zombie.target_player_id:
 				zombie.move_to_player(delta, game_stated, player)
 				break
-			pass
-		pass
-	
-	
+
+
 func _on_player_hit():
 	$UI/HitRect.visible = true
 	await get_tree().create_timer(0.1).timeout
