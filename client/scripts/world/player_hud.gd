f7d152673958fe5d97743d676aad4a3f18dc9f9d
wahthebigboi <auyeungjack123@gmail.com>
2024-04-16 00:54:28 +0800

    Update player hud scene and script

--- client/scripts/world/player_hud.gd
+++ client/scripts/world/player_hud.gd
@@ -1,31 +1,26 @@
 extends Control
 
+@export var player_id: String
+@export var player_name: String
+@export var player_number: String
+
 var heart_full = preload("res://assets/hud/heart_full.png")
 var heart_empty = preload("res://assets/hud/heart_emptied.png")
-var player_number
-var player_name
+var health = 5
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass  # Replace with function body.
-
-
-func set_info(player_name: String, player_num):
-	player_name = player_name
-	player_number = "P" + str(player_num)
+func set_info(user_id, username: String, number):
+	player_id = user_id
+	player_name = username
+	player_number = "P" + str(number)
 	$Background/PlayerName.text = player_name
 	$Background/PlayerNumber.text = player_number
 
 
-func update_health(value):
-	for i in $HealthBar.get_child_count():
-		if value > i:
-			$HealthBar.get_child(i).texture = heart_full
+func update_health():
+	health -= 1
+	for i in $Background/HealthBar.get_child_count():
+		if health > i:
+			$Background/HealthBar.get_child(i).texture = heart_full
 		else:
-			$HealthBar.get_child(i).texture = heart_empty
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+			$Background/HealthBar.get_child(i).texture = heart_empty
