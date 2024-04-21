679a598c9379ef2e3e44ce00c3e017ee538d6a31
wahthebigboi <auyeungjack123@gmail.com>
2024-04-22 02:19:16 +0800

    Update player HUD logic

--- client/scripts/world/player_hud.gd
+++ client/scripts/world/player_hud.gd
@@ -3,38 +3,35 @@ extends Control
 @export var player_id: String
 @export var player_name: String
 @export var player_number: String
+@export var player_health: int
 
 var heart_full = preload("res://assets/hud/heart_full.png")
 var heart_empty = preload("res://assets/hud/heart_emptied.png")
-var health = 5
 
 
-func set_info(user_id, username: String, number):
+func set_info(user_id, username: String, number, health):
 	player_id = user_id
 	player_name = username
 	player_number = "P" + str(number)
+	player_health = health
 	$Background/PlayerName.text = player_name
 	$Background/PlayerNumber.text = player_number
 
 
-func get_health():
-	#Fetch health from worker code to be added
-	return health
-
-
 func update_health(health):
 	for i in $Background/HealthBar.get_child_count():
-		if health > i:
+		if health >= i:
 			$Background/HealthBar.get_child(i).texture = heart_full
 		else:
 			$Background/HealthBar.get_child(i).texture = heart_empty
 
 
 func minus_health():
-	var health = get_health()
-	update_health(health - 1)
+	print(player_health)
+	player_health -= 1
+	update_health(player_health - 1)
 
 
 func add_health():
-	var health = get_health()
-	update_health(health + 1)
+	player_health += 1
+	update_health(player_health + 1)
